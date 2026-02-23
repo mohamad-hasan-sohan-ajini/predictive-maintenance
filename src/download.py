95228548219a80d785d29b6da9c6cfdb8e295293
Mohammad Hassan Sohan Ajini <mohamad.hasan.sohan.ajini@gmail.com>
2026-02-23 20:44:10 +0000

    Add nyiso raw data downloader script

---
+++ src/download.py
@@ -0,0 +1,31 @@
+from multiprocessing import Pool
+from pathlib import Path
+
+import requests
+from tqdm import tqdm
+
+base_path = Path("downloads")
+base_path.mkdir(exist_ok=True)
+
+
+def main(urls):
+    with Pool(4) as pool:
+        for resp in tqdm(pool.imap_unordered(requests.get, urls), total=len(urls)):
+            if resp is None:
+                continue
+            if resp.status_code != 200:
+                print(f"HTTP {resp.status_code}: {resp.url}")
+                continue
+
+            fname = resp.url.split("/")[-1] or "download.bin"
+            path = base_path / fname
+            path.write_bytes(resp.content)
+
+
+if __name__ == "__main__":
+    urls = [
+        f"https://mis.nyiso.com/public/csv/realtimelineoutages/{year}{month:02d}01RTLineOutages_csv.zip"
+        for year in range(2008, 2026)
+        for month in range(1, 13)
+    ]
+    main(urls)

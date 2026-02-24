12f27ab276f4ea26cbd6dc18b349b045ff4b6454
Mohammad Hassan Sohan Ajini <mohamad.hasan.sohan.ajini@gmail.com>
2026-02-24 14:04:48 +0000

    Download both actual and schedule outages

--- src/download.py
+++ src/download.py
@@ -4,11 +4,8 @@ from pathlib import Path
 import requests
 from tqdm import tqdm
 
-base_path = Path("raw-zip")
-base_path.mkdir(exist_ok=True)
 
-
-def main(urls):
+def main(urls, base_path):
     with Pool(4) as pool:
         for resp in tqdm(pool.imap_unordered(requests.get, urls), total=len(urls)):
             if resp is None:
@@ -17,15 +14,26 @@ def main(urls):
                 print(f"HTTP {resp.status_code}: {resp.url}")
                 continue
 
-            fname = resp.url.split("/")[-1] or "download.bin"
+            fname = resp.url.split("/")[-1]
             path = base_path / fname
             path.write_bytes(resp.content)
 
 
 if __name__ == "__main__":
-    urls = [
+    actual_outages_urls = [
         f"https://mis.nyiso.com/public/csv/realtimelineoutages/{year}{month:02d}01RTLineOutages_csv.zip"
         for year in range(2008, 2027)
         for month in range(1, 13)
     ]
-    main(urls)
+    actual_outages_base_path = Path("raw-zip")
+    actual_outages_base_path.mkdir(exist_ok=True)
+    main(actual_outages_urls, actual_outages_base_path)
+
+    scheduled_outages_urls = [
+        f"http://mis.nyiso.com/public/csv/schedlineoutages/{year}{month:02d}01SCLineOutages_csv.zip"
+        for year in range(2008, 2027)
+        for month in range(1, 13)
+    ]
+    scheduled_outages_base_path = Path("raw-zip-scheduled")
+    scheduled_outages_base_path.mkdir(exist_ok=True)
+    main(scheduled_outages_urls, scheduled_outages_base_path)
